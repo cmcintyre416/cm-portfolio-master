95e373128ae3733fdb629b6f43c057efa42a639e
Colin McIntyre <35696965+cmcintyre416@users.noreply.github.com>
2019-02-14 13:21:57 -0500

    Update src/templates/blogTemplate.js

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -6,7 +6,7 @@ export default function Template({
 }) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
-  console.log(data);
+
   return (
     <div className="blog-post-container">
       <div className="blog-post">

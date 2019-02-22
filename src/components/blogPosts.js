62d181bc4ffd2dd0447a8c94467a2ca744c1657a
Colin McIntyre <35696965+cmcintyre416@users.noreply.github.com>
2019-02-22 16:23:44 -0500

    Made image and posts page work.

--- src/components/blogPosts.js
+++ src/components/blogPosts.js
@@ -5,20 +5,48 @@ import styled from 'styled-components';
 const PostContainer = styled.div`
     background-color: white;
     box-shadow: 0px 2px 3px grey;
-    padding: 30px;
+    width: 30%;
+
+    a {
+      width: 100%;
+    }
+
+    img {
+      width: 100%;
+      margin-bottom: 0;
+    }
+`;
+
+const PostTextWrapper = styled.div`
+  padding: 25px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const PostDate = styled.div`
 `;
 
+const PostTitle = styled.div`
+`;
+
+const PostDescription = styled.div`
+`;
+
 const PostLink = ({ post }) => (
   <PostContainer>
     <Link to={post.frontmatter.path}>
-      <PostDate>
-        {post.frontmatter.date}
-      </PostDate>
-      {post.frontmatter.title}
-      {post.excerpt}
+      <img src={post.frontmatter.image}/>
+      <PostTextWrapper>
+        <PostDate>
+          {post.frontmatter.date}
+        </PostDate>
+        <PostTitle>
+          {post.frontmatter.title}
+        </PostTitle>
+        <PostDescription>
+          {post.excerpt}
+        </PostDescription>
+      </PostTextWrapper>
     </Link>
   </PostContainer>
 )

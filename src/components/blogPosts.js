03d4ba9424ca77350dbf37e7a50eb6fa81f9ac25
Colin Mcintyre <colin@colinmcintyre.io>
2019-02-23 10:34:18 -0500

    Revert "Merge pull request #6 from cmcintyre416/develop"
    This reverts commit 330345bacda2e6fc217e7ddb845ddbc993f1c1e2, reversing
    changes made to 928523650369421a68b4be6b192546a25d4b8533.

--- src/components/blogPosts.js
+++ src/components/blogPosts.js
@@ -5,48 +5,20 @@ import styled from 'styled-components';
 const PostContainer = styled.div`
     background-color: white;
     box-shadow: 0px 2px 3px grey;
-    width: 30%;
-
-    a {
-      width: 100%;
-    }
-
-    img {
-      width: 100%;
-      margin-bottom: 0;
-    }
-`;
-
-const PostTextWrapper = styled.div`
-  padding: 25px;
-  display: flex;
-  flex-direction: column;
+    padding: 30px;
 `;
 
 const PostDate = styled.div`
 `;
 
-const PostTitle = styled.div`
-`;
-
-const PostDescription = styled.div`
-`;
-
 const PostLink = ({ post }) => (
   <PostContainer>
     <Link to={post.frontmatter.path}>
-      <img src={post.frontmatter.image}/>
-      <PostTextWrapper>
-        <PostDate>
-          {post.frontmatter.date}
-        </PostDate>
-        <PostTitle>
-          {post.frontmatter.title}
-        </PostTitle>
-        <PostDescription>
-          {post.excerpt}
-        </PostDescription>
-      </PostTextWrapper>
+      <PostDate>
+        {post.frontmatter.date}
+      </PostDate>
+      {post.frontmatter.title}
+      {post.excerpt}
     </Link>
   </PostContainer>
 )

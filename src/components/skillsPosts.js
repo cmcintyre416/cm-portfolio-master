6eec6263953f66fd2a4e7db1586dccd69da5917a
Colin McIntyre <35696965+cmcintyre416@users.noreply.github.com>
2019-03-21 12:11:01 -0400

    Added styling

--- src/components/skillsPosts.js
+++ src/components/skillsPosts.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -10,13 +10,40 @@ library.add(faReact, faJs, faCss3);
 
 
 const SkillContainer = styled.div`
-    background-color: white;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-    border-radius: 5px;
 
-    &:hover {
-      box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    .skillLogoWrapper {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 15px;
+      background-color: white;
+      box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+      transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+      border-radius: 5px;
+
+      &:hover {
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+        cursor: pointer;
+      }
+
+      svg {
+        font-size: 2.5rem;
+      }
+
+      p {
+        padding: 0;
+        margin: 0;
+        font-size: 0.85rem;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        margin-top: 10px;
+      }
+
+    }
+
+    .closeView {
+      display: none;
     }
 
 `;
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -6,12 +6,42 @@ import BackHome from '../components/backHome';
 import Header from '../components/header';
 import PageTitles from '../components/pageTitles';
 import SkillsPosts from '../components/skillsPosts';
+import { darkHoverBlue } from '../styles/colours';
 
 import { MainWrapper, ContentWrapper } from '../styles/containers';
 import styled from 'styled-components';
 
-const SkillsPage = styled.div`
+const SkillsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  column-gap: 30px;
+  row-gap: 30px;
+`;
 
+const SkillsFilters = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 50px;
+  position: relative;
+
+  .selectorContainer {
+    label {
+      margin-right: 20px;
+    }
+  }
+
+  form {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+
+    label {
+      margin-right: 20px;
+    }
+  }
 `;
 
 const Skills = ({data}) => {
@@ -19,17 +49,32 @@ const Skills = ({data}) => {
     .map(edge => <SkillsPosts key={edge.node.id} skill={edge.node}/>)
   return (
     <>
-      <SkillsPage>
-        <Header headerName="simpleHeader" linkTo="/"/>
-        <Nav navType="fixedNav"/>
-        <MainWrapper>
-          <ContentWrapper>
-            <BackHome/>
-            <PageTitles pageTitle='Skills'/>
-            {Skills}
-          </ContentWrapper>
-        </MainWrapper>
-      </SkillsPage>
+      <Header headerName="simpleHeader" linkTo="/"/>
+      <Nav navType="fixedNav"/>
+      <MainWrapper>
+        <ContentWrapper>
+          <BackHome/>
+          <PageTitles pageTitle='Skills'/>
+          <SkillsFilters>
+            <div className="selectorContainer">
+              <label for="selectSkillTag">Skill Tag</label>
+              <select name="selectSkillTag">
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+              </select>
+            </div>
+            <form id="skillForm">
+              <label for="searchSkill">Skill Tag</label>
+              <input type="text" name="searchSkill"/>
+            </form>
+          </SkillsFilters>
+          <SkillsContainer>
+            {Skills} 
+          </SkillsContainer>
+        </ContentWrapper>
+      </MainWrapper>
     </>
   );
 }

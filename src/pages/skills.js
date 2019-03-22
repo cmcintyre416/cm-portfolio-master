f7ffc03cb7441e701225a2aa9eb214f657ed4237
Colin McIntyre <35696965+cmcintyre416@users.noreply.github.com>
2019-03-22 14:37:43 -0400

    Made tags data file and also made search filter work with useState.

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { graphql } from 'gatsby';
+import { useState } from 'react';
 
 import Nav from '../components/nav';
 import BackHome from '../components/backHome';
 import Header from '../components/header';
 import PageTitles from '../components/pageTitles';
 import SkillsPosts from '../components/skillsPosts';
+import { skillTags } from "../../static/data/skillTags";
 
 import { MainWrapper, ContentWrapper } from '../styles/containers';
 import styled from 'styled-components';
@@ -44,8 +46,19 @@ const SkillsFilters = styled.div`
 `;
 
 const Skills = ({data}) => {
-  const Skills = data.allSkillsJson.edges
-    .map(edge => <SkillsPosts key={edge.node.id} skill={edge.node}/>)
+  const [filterText, setFilter] = useState('');
+  const [initialList, setInitialList] = useState(data.allSkillsJson.edges);
+  
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
+  };
+  
+  const filteredList = initialList.filter(item => {
+    return item.node.title.toLowerCase().includes(filterText.toLowerCase());
+  });
+
+  const Skills = filteredList
+  .map(edge => <SkillsPosts key={edge.node.id} skill={edge.node}/>);
   return (
     <>
       <Header headerName="simpleHeader" linkTo="/"/>
@@ -58,15 +71,12 @@ const Skills = ({data}) => {
             <div className="selectorContainer">
               <label htmlFor="selectSkillTag">Skill Tag</label>
               <select name="selectSkillTag">
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+                {skillTags.map(tag => <option value={tag.value}>{tag.label}</option>)}
               </select>
             </div>
             <form id="skillForm">
               <label htmlFor="searchSkill">Skill Tag</label>
-              <input type="text" name="searchSkill"/>
+              <input type="text" name="searchSkill" onChange={handleFilterChange} value={filterText}/>
             </form>
           </SkillsFilters>
           <SkillsContainer>
---
+++ static/data/skillTags.js
@@ -0,0 +1,7 @@
+export const skillTags = [
+    { label: 'CSS', value: 'css' },
+    { label: 'JS', value: 'js' },
+    { label: 'Tools', value: 'tools' },
+    { label: 'Systems', value: 'systems' },
+    { label: 'Other', value: 'other' },
+];

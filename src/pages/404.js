6b7e3253c04976fe4c9b9dde0efb22142bc332fc
Colin McIntyre <35696965+cmcintyre416@users.noreply.github.com>
2019-03-29 12:17:42 -0400

    Quick 404 page build.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,31 @@
 import React from "react";
 import SEO from "../components/seo";
+import Nav from '../components/nav';
+import Header from '../components/header';
+import { MainWrapper, ContentWrapper } from '../styles/containers';
+import BackHome from '../components/backHome';
+
+import styled from 'styled-components';
+import {darkHoverBlue, darkBlue} from '../styles/colours';
+
+const NotFound = styled.div`
+  margin-top: 100px;
+  padding-top: 50px;
+`;
 
 const NotFoundPage = () => (
   <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <SEO title="404: Not found" />
+    <Header linkTo="/blog"/>
+    <MainWrapper>
+      <ContentWrapper>
+        <BackHome/>
+        <NotFound>
+            <h2>PAGE NOT FOUND</h2>
+            <p>You just hit a route that doesn't exist.</p>
+        </NotFound>
+      </ContentWrapper>
+    </MainWrapper>
   </>
 )
 

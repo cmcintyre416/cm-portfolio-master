ae2b7f55d2fb380095a17782260485121e06724c
Colin McIntyre <35696965+cmcintyre416@users.noreply.github.com>
2019-05-22 17:13:34 -0400

    Working on custom carousel component.

--- src/components/gitEvents.js
+++ src/components/gitEvents.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useState, useEffect } from 'react';
 
-import { GitEventsContainer } from "../styles/gitevents.js";
+import { GitEventsContainer, GitEventTitle, GitEventTracker } from "../styles/gitevents.js";
 
 const GitEvents = ({event}) => {
   const [events, setEvents] = useState([]);
+  const [commitNumber, setCommitNumber] = useState(0);
 
   useEffect(() => {
     getEvents();
@@ -16,33 +17,55 @@ const GitEvents = ({event}) => {
     setEvents(data.recentGithubEvents.gitEvents[0].githubActivity);
   };
 
-  const listCommits = (commits) => {
-    if(commits){
-      commits.map((commit) => {
-        console.log(commit.message);
-        return <p>{commit.message}</p>;
-      })
+  const scrollActivity = (e) => {
+    let direction = e.target.attributes.getNamedItem('direction').value;
+    console.log(events.length);
+    if(direction === 'left' && commitNumber > 0){
+      console.log('left');
+      setCommitNumber(commitNumber + 1);
+      changeDial();
+    }else if(direction === 'right' && commitNumber < events.length){
+      console.log('right');
+      setCommitNumber(commitNumber - 1);
+      changeDial();
     }else{
-      return <div>no commits</div>;
-    }
+      setCommitNumber(commitNumber);
+    };
+  };
 
+  const changeDial = () => {
+    const gitEventsContainer = document.querySelector('.gitEventsOverflow');
+    const translateAmount = commitNumber * 100;
+    console.log(translateAmount);
+    gitEventsContainer.style.transform = `translate(calc(${translateAmount}% + 20px))`;
   };
 
   return (
     <>
-      <GitEventsContainer>
-        {events.map((event)=> {
-          return (
-            <div className="gitEvents__event" key={`key-${event.id}`}>
-              <p>Activity Type: {event.type}</p>
-              <p>Repository: {event.repo.name}</p>
-              <p>Changes: {event.payload.size}</p>
-              <div><p>Commits:</p></div>
-              {listCommits(event.payload.commits)}
-            </div>
-          )
-        })}
+      <GitEventTracker>
+      <GitEventTitle>Recent Github Activity</GitEventTitle>
+      <button onClick={scrollActivity} direction="left" className="gitEvents__scroll scroll--left"></button>
+      <button onClick={scrollActivity} direction="right" className="gitEvents__scroll scroll--right"></button>
+      <GitEventsContainer id="gitEventContainer">
+        <div className="gitEventsOverflow">
+          {events.map((event)=> {
+            return (
+              <div className="gitEvents__event" key={`key-${event.id}`}>
+                <p>Activity Type: {event.type}</p>
+                <p>Repository: {event.repo.name}</p>
+                <p>Changes: {event.payload.size}</p>
+                <p>Commits:</p>
+                <div className="gitEvents__commitContainer">
+                  {event.payload.commits && event.payload.commits.map((commit, i)=>{
+                    return <p key={`eventCommit-${i}`}>{commit.message}</p>;
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </GitEventsContainer>
+      </GitEventTracker>
     </>
   );
 }
--- src/styles/gitevents.js
+++ src/styles/gitevents.js
@@ -1,17 +1,68 @@
 import styled from 'styled-components';
 
+export const GitEventTracker = styled.div`
+  position: relative;
+
+  .gitEvents__scroll {
+    position: absolute;
+    top: calc(50% + 26px);
+    transform: translateY(-50%);
+    z-index: 2;
+    padding: 20px;
+    border-radius: 50px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    cursor: pointer;
+  }
+
+  .scroll--left {
+    left: -50px;
+  }
+
+  .scroll--right {
+    right: -50px;
+  }
+
+`;
+
 export const GitEventsContainer = styled.div`
   background-color: white;
   padding: 15px 20px;
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  border-radius: 4px;
+  display: flex;
+  flex-direction: row;
+  overflow-x: hidden;
+  max-height: 400px;
+  position: relative;
+
+  .gitEventsOverflow {
+    display: flex;
+    flex-direction: row;
+    transition: 1s ease all;
+  }
 
   .gitEvents__event {
-    border-bottom: 1px solid grey;
     padding: 10px 0;
+    min-width: calc(100% + 20px);
+    padding-right: 20px;
+  }
+
+  .gitEvents__commitContainer {
+    overflow-y: scroll;
+    flex-grow: 1;
   }
 
   @media (max-width: 900px){
     margin: 0px 50px;
   }
 
+`;
+
+export const GitEventTitle = styled.p`
+  font-size: 1.2rem;
+  font-weight: 600;
+
+  @media (max-width: 900px){
+    padding: 0px 50px;
+  }
 `;
